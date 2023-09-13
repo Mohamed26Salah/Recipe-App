49391b8fea49db9ffad17c293734b31093272b00
Mohamed26Salah <76624455+Mohamed26Salah@users.noreply.github.com>
2023-09-13 16:57:24 +0300

    Finishing the app

--- iOS/Recipe-App/Recipe-App/Resources/Constants.swift
+++ iOS/Recipe-App/Recipe-App/Resources/Constants.swift
@@ -11,7 +11,8 @@ import Foundation
 struct K {
     struct Links {
         static let baseUrl = "https://tasty.p.rapidapi.com/recipes/"
-        static let apiKey = "24bfbf74f8mshd70a51277440043p144846jsn5651052b0f4b"
+        static let apiKey = "6660cd5c64mshaeb2df05c204f40p130446jsnfb36e6aee035"
+        static let apiKey2 = "24bfbf74f8mshd70a51277440043p144846jsn5651052b0f4b"
     }
     struct cellsResuable {
         static let FoodTVC = "FoodTVC"
